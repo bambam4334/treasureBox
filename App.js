2e8c58bc45ddb53e9a7f16267ec6b486dacc85a3
Phuwadech153 <56551664+Phuwadech153@users.noreply.github.com>
2021-11-22 13:54:06 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button,TouchableOpacity, Image, Linking} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button,TouchableOpacity, Image  } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Screen1 from "../screens/Screen1";
-import Screen2 from "../screens/Screen2";
-import Screen3 from "../screens/Screen3";
+
+// import screen ที่เกี่ยวข้อง
+import Backup from "./src/screen/backup"
+import ClassYear from "./src/screen/classyear";
+import HomePage from "./src/screen/homepage";
+import LecturePost from "./src/screen/lecture_post";
+import PostViewQa from "./src/screen/post_view_qa";
+import SignUp from "./src/screen/signup";
+import ViewPostLec from "./src/screen/view_post_lec";
+import QaPost from "./src/screen/QA_post";
+import MainQa from './src/screen/mainQA'; 
+import QAinfo from './src/screen/QAinfo'; 
+import MainLec from './src/screen/mainLec'; 
+import Lecinfo from './src/screen/Lecinfo'; 
+// สร้าง navigator ตามโจทย์กำหนด
+const MainNavigator = createNativeStackNavigator();
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image style={{width: 250, height:'25%', marginTop:50}} source={require("./assets/logo.png")}></Image>
-      <Text style={{fontWeight:'bold', fontSize: 30, marginTop:30, marginBottom:20}} >Welcome!</Text>
-     
-      <View>
-      <TextInput  style={styles.Email} keyboardType='email-address' placeholder='xxx@it.kmitl.ac.th' autoCapitalize='none' autoCorrect={false}/>
-      <TextInput  style={styles.Email} keyboardType='default' placeholder='Password' autoCapitalize='none' autoCorrect={false} secureTextEntry={true} />
-      </View>
+    <NavigationContainer>
+      <MainNavigator.Navigator
+          screenOptions={{
+            headerShown: false
+          }}>
+        <MainNavigator.Screen name="Backup" component={Backup}/>
+        <MainNavigator.Screen name="ClassYear" component={ClassYear}/>
+        <MainNavigator.Screen name="HomePage" component={HomePage}/>
+        <MainNavigator.Screen name="LecturePost" component={LecturePost}/>
+        <MainNavigator.Screen name="PostViewQa" component={PostViewQa}/>
+        <MainNavigator.Screen name="QaPost" component={QaPost}/>
+        <MainNavigator.Screen name="SignUp" component={SignUp}/>
+        <MainNavigator.Screen name="ViewPostLec" component={ViewPostLec}/>
+        <MainNavigator.Screen name="MainQa" component={MainQa}/>
+        <MainNavigator.Screen name="QAinfo" component={QAinfo}/>
+        <MainNavigator.Screen name="MainLec" component={MainLec}/>
+        <MainNavigator.Screen name="Lecinfo" component={Lecinfo}/>
 
-      <View style={styles.login} >
-  
-        <TouchableOpacity style={{width:160, height:60, backgroundColor:'#d9001b', borderRadius:5, margin:10}} >
-        <Text style={{fontSize:22, color:'#fff', textAlign:'center', paddingTop:15, fontWeight:'bold'}} >Sign In</Text>
-      </TouchableOpacity>
-  
-        <TouchableOpacity style={{width:110, height:60, backgroundColor:'#000', borderRadius:5, margin:10}} >
-        <Text style={{fontSize:16, color:'#fff', textAlign:'center', paddingTop:20, fontWeight:'bold', justifyContent:'center'}} >Sign Up</Text>
-      </TouchableOpacity>
-
-      </View>
-    
-    </View>
+      </MainNavigator.Navigator>
+    </NavigationContainer>
   );
 }
 
